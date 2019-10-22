f98564eb0c40622b3713e5c4cc899de71e514140
Bayturan <bayturan@yahoo.com>
2019-10-23 00:30:10 +0300

    Update odev4.c

--- Cizge Kurami/odev4.c
+++ Cizge Kurami/odev4.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #define n 3
 
-int main() {
-	int G[n][n],i,j,tepe[n];
+void main() {
+	int G[n][n],i,j,tepe[n]={0};
 	printf("Cizgenin komsuluk matrisini yaziniz:\n");
 	for(i=0;i<n;i++) {
 		for(j=0;j<n;j++) {
@@ -10,7 +10,7 @@ int main() {
 			else {
 				printf("%d ve %d tepelerinin komsuluk degerini yaziniz:",i+1,j+1);
 				scanf("%d",&G[i][j]);
-				if(G[i][j] == 1) tepe[i] += 1;
+				tepe[i] += G[i][j];
 			}
 		}
 	}
